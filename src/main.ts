997cd441055cfac2408aa13878b7a3dd0efa3fc8
Macbook <macbook@Macbooks-MacBook-Pro.local>
2024-02-16 22:08:20 +0330

    add swagger part 1

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './shared/filters/https-exeption.filter';
 import { TimeoutInterceptor } from './shared/interseptors/timeout.interseptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -20,6 +21,15 @@ async function bootstrap() {
 
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalInterceptors(new TimeoutInterceptor());
+
+  const options = new DocumentBuilder()
+    .setTitle('UFOTUBE')
+    .setDescription('Video sharing application')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
--- src/modules/video/dto/update-video.dto.ts
+++ src/modules/video/dto/update-video.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
 import { CreateVideoDto } from './create-video.dto';
 
 export class UpdateVideoDto extends PartialType(CreateVideoDto) {}
