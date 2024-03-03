6a1b4d6f2cf32ddf9df6bf7942b5767a5f4134fd
Macbook <macbook@Macbooks-MacBook-Pro.local>
2024-03-03 20:03:43 +0330

    authentication - impelement bcrypt service

--- src/modules/iam/hashing/bcrypt.service.ts
+++ src/modules/iam/hashing/bcrypt.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { HashingService } from './hashing.service';
+import { genSalt, hash, compare } from 'bcrypt';
 
 @Injectable()
-export class BcryptService {}
+export class BcryptService implements HashingService {
+  async hash(data: string | Buffer): Promise<string> {
+    const salt = await genSalt();
+    return hash(data, salt);
+  }
+  compare(data: string | Buffer, encrypted: string): Promise<boolean> {
+    return compare(data, encrypted);
+  }
+}
--- src/modules/iam/iam.module.ts
+++ src/modules/iam/iam.module.ts
@@ -3,6 +3,11 @@ import { HashingService } from './hashing/hashing.service';
 import { BcryptService } from './hashing/bcrypt.service';
 
 @Module({
-  providers: [HashingService, BcryptService]
+  providers: [
+    {
+      provide: HashingService,
+      useClass: BcryptService,
+    },
+  ],
 })
 export class IamModule {}
